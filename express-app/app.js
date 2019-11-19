e7d4931829f44b1db99ff607458379e9bf86f0c8
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-19 02:45:22 -0800

    Implement logout functionality

--- express-app/app.js
+++ express-app/app.js
@@ -6,16 +6,19 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 // dependencies
-const passport = require('passport');
 const flash = require('express-flash');
+const passport = require('passport');
+const initializePassport = require('./config/passport');
+initializePassport(passport, user => (user));
+const methodOverride = require('method-override');
 const session = require('express-session');
+
 
 // router dependencies
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
-const initializePassport = require('./config/passport');
-initializePassport(passport, user => (user));
+
 
 const app = express();
 
@@ -25,7 +28,6 @@ app.set('view engine', 'pug');
 
 // allow form data access inside req(uest) variables in POST methods
 app.use(express.urlencoded({ extended: false }));
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -42,6 +44,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
