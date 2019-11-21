7b33c3cd3e4989165d0cbc563a43598d973852c9
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-21 13:53:48 -0800

    Refactor

--- express-app/config/passport.js
+++ express-app/config/passport.js
@@ -1,42 +1,37 @@
-const bcrypt = require('bcrypt');
-const database = require('../config/database');
+const bcrypt = require("bcrypt");
+const database = require("../config/database");
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-const saltRounds = 10;
-
+const LocalStrategy = require("passport-local").Strategy;
 
 function initialize(passport, user) {
-    const authenticateUser = async (username, password, done) => {
-        try {
-            const usernameQuery = `SELECT * FROM user WHERE username='${username}';`;
+  const authenticateUser = async (username, password, done) => {
+    try {
+      const usernameQuery = `SELECT * FROM user WHERE username='${username}';`;
 
-            database.query(usernameQuery, (err, results) => {
-                if (results.length == 0) {
-                    return done(null, false, { message: 'User not found!' });
-                }
-                else {
-                    const hashed_password = results[0].hashed_password;
-                    if (bcrypt.compareSync(password, hashed_password)) {
-                        //user found
-                        return done(null, user = { username: username, id: results[0].id });
-                    }
-                    else {
-                        return done(null, false, { message: 'Incorrect password!' });
-                    }
-                }
-            })
+      database.query(usernameQuery, (err, results) => {
+        if (results.length == 0) {
+          return done(null, false, { message: "User not found!" });
+        } else {
+          const hashed_password = results[0].hashed_password;
+          if (bcrypt.compareSync(password, hashed_password)) {
+            //user found
+            return done(
+              null,
+              (user = { username: username, id: results[0].id })
+            );
+          } else {
+            return done(null, false, { message: "Incorrect password!" });
+          }
         }
-
-        catch (err) {
-            console.error(err.stack);
-        }
+      });
+    } catch (err) {
+      console.error(err.stack);
     }
+  };
 
-    passport.use(new LocalStrategy({}, authenticateUser));
-    passport.serializeUser((user, done) => done(null, user));
-    passport.deserializeUser((user, done) => done(null, user));
+  passport.use(new LocalStrategy({}, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
 }
 
 module.exports = initialize;
