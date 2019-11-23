3794995fea54e97fe8a957198c4357fe2bcaf838
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-23 06:27:03 -0800

    Add initial edit-attire form
    Initial values dynamically change based on chosen attire

--- express-app/routes/edit-attire.js
+++ express-app/routes/edit-attire.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
+const database = require("../config/database");
+
 /* GET edit-attire page. */
 router.get("/", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("edit-attire");
+    const query = `
+            SELECT 
+                type, attire_name, brand, color, size
+            FROM
+                user
+                    JOIN
+                owned_by USING (username)
+		            JOIN
+	            attire_contained_by_closet USING (closet_id)
+		            JOIN
+	            attire USING (attire_id)
+            WHERE
+            username = '${req.user.username}';`;
+
+    database.query(query, (error, results) => {
+      if (error) {
+        let messages = { error: error };
+        res.redirect("/closet", { messages });
+      } else {
+        res.render("edit-attire", { results : results });
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+/* POST edit-attire page. */
+router.post("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.redirect("/closet");
   } else {
     res.redirect("/");
   }
