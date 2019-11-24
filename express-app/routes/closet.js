23c3e52895977b645aa6703b049b95b9417b3451
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-24 05:01:20 -0800

    Simplify closet error handling

--- express-app/routes/closet.js
+++ express-app/routes/closet.js
@@ -5,32 +5,31 @@ const database = require("../config/database");
 
 /* GET closet page */
 router.get("/", (req, res) => {
-  if (req.isAuthenticated()) {
-    try {
-      const query = `
-            SELECT 
-                type, attire_name, brand, color, size
-            FROM
-                user
-                    JOIN
-                owned_by USING (username)
-		            JOIN
-	            attire_contained_by_closet USING (closet_id)
-		            JOIN
-	            attire USING (attire_id)
-            WHERE
-            username = '${req.user.username}';`;
-
-      database.query(query, (err, results) => {
-        if (err) throw err;
-        res.render("closet", { user: req.user, results: results });
-      });
-    } catch (err) {
-      console.error(err.stack);
-      res.redirect("/index");
-    }
-  } else {
+  if (!req.isAuthenticated()) {
     res.redirect("/");
+  } else {
+    const query = `
+    SELECT 
+      type, attire_name, brand, color, size
+    FROM
+      user
+        JOIN
+      owned_by USING (username)
+        JOIN
+      attire_contained_by_closet USING (closet_id)
+        JOIN
+      attire USING (attire_id)
+    WHERE
+      username = '${req.user.username}';`;
+
+    database.query(query, (error, results) => {
+      if (error) {
+        let messages = { error: error };
+        res.render("closet", { messages });
+      } else {
+        res.render("closet", { results });
+      }
+    });
   }
 });
 
