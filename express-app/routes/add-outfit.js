69cef5ac4192bae1607c1cf7859d887d4d1af298
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-29 03:13:12 -0800

    Add add-outfit form

--- express-app/routes/add-outfit.js
+++ express-app/routes/add-outfit.js
@@ -5,16 +5,24 @@ const database = require("../config/database");
 
 /* GET add-outfit page */
 router.get("/", (req, res) => {
-    if (!req.isAuthenticated()) {
-        res.redirect("/");
-    } else {
-        try {
-            res.render("add-outfit");
-        } catch (error) {
-            res.render("add-outfit", {messages: error});
+  if (!req.isAuthenticated()) {
+    res.redirect("/");
+  } else {
+    try {
+      const username = req.user.username;
+      let closetQuery = `SELECT * FROM closet JOIN owned_by USING (closet_id) WHERE username = '${username}';`;
+      database.query(closetQuery, (error, closets) => {
+        if (error) {
+          throw error;
+        } else {
+          closets = JSON.parse(JSON.stringify(closets));
+          res.render("add-outfit", { closets });
         }
-
+      });
+    } catch (error) {
+      res.render("add-outfit", { messages: error });
     }
-})
+  }
+});
 
 module.exports = router;
