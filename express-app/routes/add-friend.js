55943d60fc7ca4682533c76cc0fee163997ae7f9
DJGonzales96 <devin.j.gonzales@gmail.com>
2019-11-29 19:41:23 -0800

    small fix for add-friend

--- express-app/routes/add-friend.js
+++ express-app/routes/add-friend.js
@@ -7,7 +7,7 @@ const database = require("../config/database");
 /* GET add-friend page */
 router.get("/", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("add-attire");
+    res.render("add-friend");
   } else {
     res.redirect("/");
   }
