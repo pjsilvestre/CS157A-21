cbd2c1f673a5d373977b70fda6cb13afff0d8e22
DJGonzales96 <devin.j.gonzales@gmail.com>
2019-11-29 19:41:58 -0800

    worn-list routing functionality implemented

--- express-app/routes/remove-worn.js
+++ express-app/routes/remove-worn.js
@@ -1,9 +1,48 @@
+//TODO: remove-worn.pug
 const express = require("express");
 const router = express.Router();
 
 const database = require("../config/database");
 
+/* GET remove-worn page */
+router.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("remove-worn");
+  } else {
+    res.redirect("/");
+  }
+});
 
+/* POST add-friend page, redirecting to friends list*/
+router.post("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    try {
+      const username = req.user.username;
+      //TODO: add outfit_name field in remove-worn.pug
+      const outfit_name = req.body.outfit_name;
+      //TODO: add date field in remove-worn.pug
+      const date = req.body.date;
+
+      //remove worn attire from user's list
+      let query = `DELETE FROM worn_by WHERE
+    	  			username = '${username}',
+    	  			outfit_name = '${outfit_name}'
+    	  			date = '${date}';`;
+
+      database.query(query, err => {
+        if (err) throw err;
+      });
+    });
+    } catch (err) {
+      console.error(err.stack);
+      res.redirect("/remove-worn");
+    } finally {
+      res.redirect("/worn-list");
+    }
+  } else {
+    res.redirect("/");
+  }
+});
 
 
 
--- express-app/routes/worn-list.js
+++ express-app/routes/worn-list.js
@@ -1,9 +1,34 @@
+//TODO: worn-list.pug
 const express = require("express");
 const router = express.Router();
 
 const database = require("../config/database");
 
+/* GET worn list page */
+router.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    try {
+    	
+      const query = `
+            SELECT 
+                outfit_name, date
+            FROM
+                worn_by
+            WHERE
+            username = '${req.user.username}';`;
 
+      database.query(query, (err, results) => {
+        if (err) throw err;
+        res.render("worn-list", { user: req.user, results: results });
+      });
+    } catch (err) {
+      console.error(err.stack);
+      res.redirect("/index");
+    }
+  } else {
+    res.redirect("/");
+  }
+});
 
 
 
