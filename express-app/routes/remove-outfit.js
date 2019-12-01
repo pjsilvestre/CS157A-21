30eacde5574105200a605ccc0b2aa9b70ae9798f
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-12-01 01:57:58 -0800

    Add closet selection

--- express-app/routes/remove-outfit.js
+++ express-app/routes/remove-outfit.js
@@ -8,7 +8,21 @@ router.get('/', (req, res) => {
   if (!req.isAuthenticated()) {
     res.redirect('/');
   } else {
-    res.render('remove-outfit');
+    try {
+      const username = req.user.username;
+      const closetQuery = `SELECT * FROM closet JOIN owned_by USING (closet_id) WHERE username = '${username}';`;
+      database.query(closetQuery, (error, closets) => {
+        if (error) {
+          throw error;
+        } else {
+          closets = JSON.parse(JSON.stringify(closets));
+
+          res.render('remove-outfit', { closets });
+        }
+      });
+    } catch (error) {
+      res.render('remove-outfit', { messages: error });
+    }
   }
 });
 
