5448018b1d803cfc91d7405f967a0b4d5e4daaa5
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-12-02 22:43:09 -0800

    Fix remove-attire attire display

--- express-app/routes/remove-attire.js
+++ express-app/routes/remove-attire.js
@@ -8,45 +8,51 @@ router.get('/', (req, res) => {
   if (!req.isAuthenticated()) {
     res.redirect('/');
   } else {
-    const username = req.user.username;
-    const closetQuery = `SELECT * FROM closet JOIN owned_by USING (closet_id) WHERE username = '${username}';`;
-    database.query(closetQuery, (error, closets) => {
-      if (error) {
-        throw error;
-      } else {
-        closets = JSON.parse(JSON.stringify(closets));
-      }
-      const query = `
-              SELECT 
-                  attire_id
-              FROM
-                  user
-                      JOIN
-                  owned_by USING (username)
-                  JOIN
-                attire_contained_by_closet USING (closet_id)
-                  JOIN
-                attire USING (attire_id)
-              WHERE
+    try {
+      const username = req.user.username;
+      const closetQuery = `SELECT * FROM closet JOIN owned_by USING (closet_id) WHERE username = '${username}';`;
+
+      database.query(closetQuery, (error, closets) => {
+        if (error) {
+          throw error;
+        } else {
+          closets = JSON.parse(JSON.stringify(closets));
+
+          const query = `
+            SELECT 
+                attire_id, attire_name, closet_id
+            FROM
+              user
+                JOIN
+              owned_by USING (username)
+                JOIN
+              attire_contained_by_closet USING (closet_id)
+                JOIN
+              attire USING (attire_id)
+            WHERE
               username = '${username}';`;
 
-      database.query(query, (error, attires) => {
-        if (error) {
-          let messages = { error: error };
-          res.redirect('/closet', { messages });
-        } else {
-          attires = JSON.parse(JSON.stringify(attires));
-          res.render('remove-attire', { closets, attires });
+          database.query(query, (error, attire) => {
+            if (error) {
+              throw error;
+            } else {
+              attire = JSON.parse(JSON.stringify(attire));
+              res.render('remove-attire', { closets, attire });
+              console.log({ closets, attire });
+            }
+          });
         }
       });
-    });
+    } catch (error) {
+      res.render('index', { messages: error });
+    }
   }
 });
 
-/* POST remove-attire page. */
+/* POST remove-attire page TODO*/
 router.post('/', (req, res) => {
   if (req.isAuthenticated()) {
-    const attireID = req.body.attireID;
+    const attire_id = req.body.attire_id;
 
     let query = `DELETE FROM attire
                  WHERE attire_id=${attireID};`;
